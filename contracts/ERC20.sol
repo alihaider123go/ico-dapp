92f155e24713122e34189d3eaa8895e85f169662
Ali Haider <alihaider123go@gmail.com>
2024-10-22 14:46:41 +0500

    Add openzeppelin lib

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -2,3 +2,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TecXraToken is ERC20 {
+    constructor() ERC20("TecXra Token", "@TXT"){
+        _mint(msg.sender, 1000000000000000000000000);
+    }
+}
